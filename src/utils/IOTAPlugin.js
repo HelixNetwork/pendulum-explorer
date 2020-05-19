6f1192dde865c867021f80ba0dedf0bafb810181
sachu shaji abraham <sachushajiabraham@gmail.com>
2020-05-19 11:52:47 +0530

    Fixed lint errors for iotaPlugin

--- src/utils/IOTAPlugin.js
+++ src/utils/IOTAPlugin.js
@@ -1,29 +1,24 @@
-const moment = require('moment')
-require('@/lib/iota')
-const iotaNode = require("@/utils/iota-node")
+const moment = require('moment');
+require('@/lib/iota');
+const iotaNode = require('@/utils/iota-node');
 
 export default {
   install(Vue, options) {
-    Vue.prototype.$localeTimestamp = function (timestamp) {
-      return moment(timestamp * 1000).format()
-    }
+    Vue.prototype.$localeTimestamp = timestamp => moment(timestamp * 1000).format();
 
-    Vue.prototype.$getQRCode = function (address) {
-      var json = {
+    Vue.prototype.$getQRCode = (address) => {
+      const json = {
         address,
         amount: '',
-        message: ''
-      }
-      return JSON.stringify(json)
-    }
+        message: '',
+      };
+      return JSON.stringify(json);
+    };
 
-    Vue.prototype.$getStyleIO = function (h1, h2) {
-      if (h1 === null || h2 === null) {
-        return
+    Vue.prototype.$getStyleIO = (h1, h2) => {
+      if (!(h1 === null || h2 === null) && iotaNode.iota.utils.noChecksum(h1) === iotaNode.iota.utils.noChecksum(h2)) {
+        return 'font-weight: bold; font-style:italic';
       }
-      if (iotaNode.iota.utils.noChecksum(h1) === iotaNode.iota.utils.noChecksum(h2)) {
-        return 'font-weight: bold; font-style:italic'
-      }
-    }
-  }
-}
+    };
+  },
+};
