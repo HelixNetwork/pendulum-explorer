7d8c0288652d0e538332fd2d05cd66f692b6b42c
sachu shaji abraham <sachushajiabraham@gmail.com>
2020-05-19 11:27:01 +0530

    fixing linter problem

--- src/directives/click-outside.js
+++ src/directives/click-outside.js
@@ -1,16 +1,18 @@
 import Vue from 'vue';
+
 Vue.directive('click-outside', {
-  bind: function (el, binding, vnode) {
-    el.event = function (event) {
+  bind(el, binding, vnode) {
+    // eslint-disable-next-line no-param-reassign
+    el.event = (event) => {
       // here I check that click was outside the el and his childrens
-      if (!(el == event.target || el.contains(event.target))) {
+      if (!(el === event.target || el.contains(event.target))) {
         // and if it did, call method provided in attribute value
         vnode.context[binding.expression](event);
       }
     };
-    document.body.addEventListener('click', el.event)
+    document.body.addEventListener('click', el.event);
   },
-  unbind: function (el) {
-    document.body.removeEventListener('click', el.event)
+  unbind(el) {
+    document.body.removeEventListener('click', el.event);
   },
 });
--- src/directives/toast.js
+++ src/directives/toast.js
@@ -1,17 +1,18 @@
 import Vue from 'vue';
+
 Vue.directive('toast', {
-  bind: function (el, binding, vnode) {    
-    el.event = function(event)
-    {        
-        if(el == event.target || el.contains(event.target))
-        {
-            Vue.toast(binding.expression.replace(/"/g,''),{className:['toast-box'],verticalPosition: 'bottom', duration: 3000})
-        }
-    }
-    document.body.addEventListener('click', el.event)    
+  // eslint-disable-next-line no-unused-vars
+  bind(el, binding, _vnode) {
+    // eslint-disable-next-line no-param-reassign
+    el.event = (event) => {
+      if (el === event.target || el.contains(event.target)) {
+        Vue.toast(binding.expression.replace(/"/g, ''), { className: ['toast-box'], verticalPosition: 'bottom', duration: 3000 });
+      }
+    };
+    document.body.addEventListener('click', el.event);
   },
-  unbind: function (el) {
-    document.body.removeEventListener('click', el.event)
-  }
-})
+  unbind(el) {
+    document.body.removeEventListener('click', el.event);
+  },
+});
 
