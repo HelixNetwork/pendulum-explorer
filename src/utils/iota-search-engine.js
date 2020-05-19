e246fb190076dcc818b53a6055a74b350dfb26d2
sachu shaji abraham <sachushajiabraham@gmail.com>
2020-05-19 11:46:58 +0530

    Fixed lint errors for ioa-search-engine

--- src/utils/iota-search-engine.js
+++ src/utils/iota-search-engine.js
@@ -1,46 +1,44 @@
-require('@/lib/iota')
-const iotaNode = require("@/utils/iota-node")
-const _ = require('lodash')
+require('@/lib/iota');
+const iotaNode = require('@/utils/iota-node');
+const _ = require('lodash');
 
 module.exports = (val, callbackTxs, callbackAddresses, callbackBundles, fullyDone) => {
-  var callbackStack = 3
-  var callbackCheck = () => {
-    callbackStack--
-    if(callbackStack === 0) {
-      if(fullyDone !== undefined) {
-        fullyDone()
+  let callbackStack = 3;
+  const callbackCheck = () => {
+    callbackStack--;
+    if (callbackStack === 0) {
+      if (fullyDone !== undefined) {
+        fullyDone();
       }
     }
-  }
+  };
 
-  iotaNode.iota.api.getBalances([val], 20, function(e, r) {
-    if(r !== undefined) {
-      var balance = parseInt(r.balances[0])
-      iotaNode.iota.api.findTransactionObjects({ addresses: [val] }, function(e, r) {
-        if(r !== undefined && r.length > 0) {
+  iotaNode.iota.api.getBalances([val], 20, (e, r) => {
+    if (r !== undefined) {
+      const balance = parseInt(r.balances[0]);
+      iotaNode.iota.api.findTransactionObjects({ addresses: [val] }, (e, r) => {
+        if (r !== undefined && r.length > 0) {
           callbackAddresses([{
             address: val,
-            balance: balance
-          }])
+            balance,
+          }]);
         }
-        callbackCheck()
-      })
+        callbackCheck();
+      });
     } else {
-      callbackCheck()
+      callbackCheck();
     }
-  })
-  iotaNode.iota.api.getTransactionsObjects([val], function(e, r) {
-    callbackTxs(_.filter(r, (tx) => {
-      return tx.hash !== '999999999999999999999999999999999999999999999999999999999999999999999999999999999'
-    }))
-    callbackCheck()
-  })
-  iotaNode.iota.api.findTransactionObjects({ bundles: [val] }, function(e, r) {
-    if(r !== undefined && r.length > 0) {
+  });
+  iotaNode.iota.api.getTransactionsObjects([val], (e, r) => {
+    callbackTxs(_.filter(r, tx => tx.hash !== '999999999999999999999999999999999999999999999999999999999999999999999999999999999'));
+    callbackCheck();
+  });
+  iotaNode.iota.api.findTransactionObjects({ bundles: [val] }, (e, r) => {
+    if (r !== undefined && r.length > 0) {
       callbackBundles([{
-        hash: r[0].bundle
-      }])
+        hash: r[0].bundle,
+      }]);
     }
-    callbackCheck()
-  })
-}
+    callbackCheck();
+  });
+};
