9cf3035b2e30e4776861d664458d0fab1800b61d
sachu shaji abraham <sachushajiabraham@gmail.com>
2020-05-29 20:40:56 +0530

    Fix address search issue

--- src/utils/helix-search-engine.js
+++ src/utils/helix-search-engine.js
@@ -1,11 +1,12 @@
 import helixNode from '@/utils/helix-node';
+import { isAddress, isTxHex } from '@helixnetwork/validators';
 
 const _ = require('lodash');
 
 const helixSearch = (val, callbackTxs, callbackAddresses, callbackBundles, fullyDone) => {
   let callbackStack = 3;
-  const callbackCheck = () => {
-    callbackStack -= 1;
+  const callbackCheck = (decrement) => {
+    callbackStack -= decrement;
     if (callbackStack === 0) {
       if (fullyDone !== undefined) {
         fullyDone();
@@ -13,35 +14,42 @@ const helixSearch = (val, callbackTxs, callbackAddresses, callbackBundles, fully
     }
   };
 
-  helixNode.helix.getBalances([val], 20, (error, response) => {
-    if (response !== undefined) {
-      const balance = parseInt(response.balances[0], 10);
-      helixNode.helix.findTransactionObjects({ addresses: [val] }, (err, resp) => {
-        if (resp !== undefined && resp.length > 0) {
-          callbackAddresses([{
-            address: val,
-            balance,
-          }]);
-        }
-        callbackCheck();
-      });
-    } else {
-      callbackCheck();
-    }
-  });
+  // Checking whether the inserted hash is a valid address and if yes
+  // show its information
+  if (isAddress(val)) {
+    helixNode.helix.getBalances([val], 20, (error, response) => {
+      if (response !== undefined) {
+        const balance = parseInt(response.balances[0], 10);
+        callbackAddresses([{
+          address: val,
+          balance,
+        }]);
+      }
+      callbackCheck(1);
+    });
+  } else {
+    callbackCheck(1);
+  }
 
-  helixNode.helix.getTransactionObjects([val], (error, response) => {
-    callbackTxs(_.filter(response, tx => tx.bundle !== '0000000000000000000000000000000000000000000000000000000000000000'));
-    callbackCheck();
-  });
-  helixNode.helix.findTransactionObjects({ bundles: [val] }, (error, response) => {
-    if (response !== undefined && response.length > 0) {
-      callbackBundles([{
-        hash: response[0].bundle,
-      }]);
-    }
-    callbackCheck();
-  });
+  // Checking whether the given hash is a valid transaction / bundle hash or not
+  // if yes, trying to findout the associated bundle / transaction
+
+  if (isTxHex(val, 64)) {
+    helixNode.helix.getTransactionObjects([val], (error, response) => {
+      callbackTxs(_.filter(response, tx => tx.bundle !== '0000000000000000000000000000000000000000000000000000000000000000'));
+      callbackCheck(1);
+    });
+    helixNode.helix.findTransactionObjects({ bundles: [val] }, (error, response) => {
+      if (response !== undefined && response.length > 0) {
+        callbackBundles([{
+          hash: response[0].bundle,
+        }]);
+      }
+      callbackCheck(1);
+    });
+  } else {
+    callbackCheck(2);
+  }
 };
 
 export default helixSearch;
