f0fa289f2625a71b182a0241f3fc78005dd0759f
sachu shaji abraham <sachushajiabraham@gmail.com>
2020-06-12 17:21:07 +0530

    Added table responsive class

--- src/pages/AddressList.vue
+++ src/pages/AddressList.vue
@@ -3,25 +3,26 @@
         <div class="row justify-content-center">
         <div class="col-sm-12 col-md-12 col-lg-12 text-left">
          <p class="blue-color">TOP {{addressList.addressList.length}} RICH ADDRESS LIST</p>
-
-  <table class="table mb-5">
+<div class="table-responsive-sm">
+  <table class="table mb-5 ">
       <thead>
           <tr>
-            <th scope="col">Rank</th>
-            <th scope="col">Address</th>
-            <th scope="col">Value</th>
-            <th scope="col">Price (USD)</th>
+            <th scope="col-2">Rank</th>
+            <th scope="col-6">Address</th>
+            <th scope="col-2">Value</th>
+            <th scope="col-2">Price (USD)</th>
          </tr>
       </thead>
       <tbody>
         <tr v-for="(address , index) in addressList.addressList">
-          <td>#{{ index+1 }}</td>
+          <td >#{{ index+1 }}</td>
           <td><router-link :to="{ name: 'Address', params: { hash: address.address }}" class="blue-color">{{ address.address }}</router-link></td>
           <td> <helix-balance-view :value='parseInt(address.value) ' :precision='true'></helix-balance-view></td>
           <td>$ {{Number(( address.value / 1000000) * helixNode.exchangePrice).toFixed(2)}} </td>
       </tr>
       </tbody>
     </table>
+</div>
         </div>
       </div>
   </div>
